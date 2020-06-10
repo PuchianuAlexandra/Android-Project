d0b667161be3196a9ad18924f236e4a4425304a9
zahariaalexandra <62277649+zahariaalexandra@users.noreply.github.com>
2020-06-10 14:23:10 +0300

    Create login fragment

--- app/src/main/java/com/example/android_project_medicinesupply/Activities/LoginActivity.java
+++ app/src/main/java/com/example/android_project_medicinesupply/Activities/LoginActivity.java
@@ -1,53 +1,22 @@
 package com.example.android_project_medicinesupply.Activities;
 
 import android.os.Bundle;
-import android.text.method.HideReturnsTransformationMethod;
-import android.text.method.PasswordTransformationMethod;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.android_project_medicinesupply.Fragments.NewAccountFragment;
+import com.example.android_project_medicinesupply.Fragments.LoginFragment;
 import com.example.android_project_medicinesupply.R;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
-
-    CheckBox checkSeePassword;
-    TextInputEditText txtPassword;
-    Button btnCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        checkSeePassword = findViewById(R.id.checkSeePassword);
-        txtPassword = findViewById(R.id.txtPassword);
-        btnCreateAccount = findViewById(R.id.btnCreateAccount);
-
-        checkSeePassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    txtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                } else {
-                    txtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                }
-            }
-        });
-
-        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.add(R.id.fragmentFrame, new NewAccountFragment());
-                transaction.commit();
-            }
-        });
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.fragmentFrame, new LoginFragment());
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/android_project_medicinesupply/Fragments/LoginFragment.java
@@ -0,0 +1,58 @@
+package com.example.android_project_medicinesupply.Fragments;
+
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.android_project_medicinesupply.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginFragment extends Fragment {
+
+    CheckBox checkSeePassword;
+    TextInputEditText txtPassword;
+    Button btnCreateAccount;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        checkSeePassword = view.findViewById(R.id.checkSeePassword);
+        txtPassword = view.findViewById(R.id.txtPassword);
+        btnCreateAccount = view.findViewById(R.id.btnCreateAccount);
+
+        checkSeePassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    txtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    txtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragmentFrame, new NewAccountFragment());
+                transaction.commit();
+            }
+        });
+
+        return view;
+    }
+
+    
+}
