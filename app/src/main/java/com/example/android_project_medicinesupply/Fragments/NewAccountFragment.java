7c270604a9e8740258a966d408e0b72efb6a3574
PuchianuAlexandra <62277662+PuchianuAlexandra@users.noreply.github.com>
2020-06-10 18:41:36 +0300

    Add cancel button functionality and validation for telephone nr.

--- app/src/main/java/com/example/android_project_medicinesupply/Fragments/NewAccountFragment.java
+++ app/src/main/java/com/example/android_project_medicinesupply/Fragments/NewAccountFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.android_project_medicinesupply.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class NewAccountFragment extends Fragment {
 
@@ -15,6 +18,46 @@ public class NewAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_new_account, container, false);
+        final View view=inflater.inflate(R.layout.fragment_new_account, container, false);
+        Button btnCreateAccount=view.findViewById(R.id.btnCreateAccount);
+        Button btnCancel=view.findViewById(R.id.btnCancel);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getActivity().getSupportFragmentManager().getBackStackEntryCount() != 0) {
+                    getActivity().getSupportFragmentManager().popBackStackImmediate();
+                }
+            }
+        });
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextInputEditText txtName=view.findViewById(R.id.txtName);
+                TextInputEditText txtTelephone=view.findViewById(R.id.txtTelephone);
+                TextInputEditText txtEmail=view.findViewById(R.id.txtEmail);
+                TextInputEditText txtPassword=view.findViewById(R.id.txtPassword);
+                TextInputEditText txtConformPassword=view.findViewById(R.id.txtConformPassword);
+                if(!validateTelephone(txtTelephone.getText().toString())){
+                    Toast toast=Toast.makeText(getContext(), R.string.invalid_telephone,Toast.LENGTH_LONG);
+                }
+            }
+        });
+        return view;
     }
+
+    private boolean validateTelephone(String telephone){
+        if(!Character.isDigit(telephone.charAt(0)) || telephone.charAt(0)!='+'){
+            return false;
+        }
+        for(int index=1;index<telephone.length();index++){
+            if(!Character.isDigit(telephone.charAt(index))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
