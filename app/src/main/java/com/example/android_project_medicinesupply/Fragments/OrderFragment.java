8405df7cbf3ceb7748f43197e19f37ee5453aa1f
zahariaalexandra <62277649+zahariaalexandra@users.noreply.github.com>
2020-06-18 22:50:32 +0300

    Populate the order recycler view

--- app/src/main/java/com/example/android_project_medicinesupply/Fragments/OrderFragment.java
+++ app/src/main/java/com/example/android_project_medicinesupply/Fragments/OrderFragment.java
@@ -6,11 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android_project_medicinesupply.Database.Medicine;
+import com.example.android_project_medicinesupply.Database.MedicineAdapter;
 import com.example.android_project_medicinesupply.Database.User;
 import com.example.android_project_medicinesupply.R;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -18,6 +23,9 @@ public class OrderFragment extends Fragment {
 
     private User user;
     private List<Medicine> medicines;
+    private RecyclerView recyclerView;
+    private MedicineAdapter medicineAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     public OrderFragment(User user, List<Medicine> medicines) {
         this.user = user;
@@ -27,7 +35,26 @@ public class OrderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        View view = inflater.inflate(R.layout.fragment_order, container, false);
+        recyclerView = view.findViewById(R.id.orderRecyclerView);
+
+        populateRecyclerView();
+        return view;
+    }
+
+    private void populateRecyclerView() {
+        if(medicines.size() > 0) {
+            Collections.sort(medicines, new Comparator<Medicine>() {
+                @Override
+                public int compare(final Medicine o1, final Medicine o2) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+        }
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        medicineAdapter = new MedicineAdapter(medicines);
+        recyclerView.setAdapter(medicineAdapter);
     }
 }
