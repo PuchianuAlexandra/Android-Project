9ba6f605808def57ff12bdb6a1c49a158440ddd7
PuchianuAlexandra <62277662+PuchianuAlexandra@users.noreply.github.com>
2020-06-19 18:12:01 +0300

    Create user fragment

--- app/src/main/java/com/example/android_project_medicinesupply/Fragments/UserFragment.java
+++ app/src/main/java/com/example/android_project_medicinesupply/Fragments/UserFragment.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.android_project_medicinesupply.Database.User;
 import com.example.android_project_medicinesupply.R;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +30,24 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+        TextView txtUserName = view.findViewById(R.id.txtUserName);
+        TextView txtTelephone = view.findViewById(R.id.txtTelephone);
+        TextView txtEmail = view.findViewById(R.id.txtEmail);
+        Button btnCancel=view.findViewById(R.id.btnCancel);
+
+        txtUserName.setText(user.getName());
+        txtTelephone.setText("Telephon: " + user.getTelephone());
+        txtEmail.setText("Email: "+user.getEmail());
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getActivity().getSupportFragmentManager().getBackStackEntryCount() != 0) {
+                    getActivity().getSupportFragmentManager().popBackStackImmediate();
+                }
+            }
+        });
+        return view;
     }
 }
